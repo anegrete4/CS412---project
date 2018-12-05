4cfea573aea499193e2661937d2110caace407a1
Keshab Panthi <panthikeshab8shreekesh@gmail.com>
2018-12-04 21:12:11 -0600

    merged changes of shopping cart

--- ground-hogs/WebContent/JavaScript/main.js
+++ ground-hogs/WebContent/JavaScript/main.js
@@ -3,14 +3,6 @@ $(document).ready(function(){
 	
 	
 	
-	var slider = document.getElementById("myRange");
-	var output = document.getElementById("demo");
-//	output.innerHTML = slider.value;
-
-	slider.oninput = function() {
-	//  output.innerHTML = this.value;
-	}
-	
 	window.onscroll = function() {scrollFunction()};
 	$(".nextSection").on('click', function(event) {  
 	    if (this.hash !== "") {
